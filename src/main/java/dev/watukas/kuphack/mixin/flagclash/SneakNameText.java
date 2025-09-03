6a677c3bac800403f9833bf27654de46fc003252
Vaapukkax <73548405+Watukas@users.noreply.github.com>
2025-09-04 01:37:36 +0300

    Make name tags visible when sneaking

--- src/main/java/dev/watukas/kuphack/mixin/flagclash/SneakNameText.java
+++ src/main/java/dev/watukas/kuphack/mixin/flagclash/SneakNameText.java
@@ -1,4 +1,4 @@
-package dev.watukas.kuphack.mixin;
+package dev.watukas.kuphack.mixin.flagclash;
 
 import java.util.UUID;
 
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import dev.watukas.kuphack.SupportedServer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.entity.EntityRendererFactory.Context;
@@ -21,23 +22,31 @@ import net.minecraft.util.math.Vec3d;
  * You could say I was bribed, but instead I'd say I was gifted something so I paid him back :)
  */
 @Mixin(PlayerEntityRenderer.class)
-public abstract class VinueText extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityRenderState, PlayerEntityModel> {
+public abstract class SneakNameText extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityRenderState, PlayerEntityModel> {
 	
 	private final UUID uuid = UUID.fromString("d76c3884-1ead-40a9-8f65-461d3b5264e2");
 	
-	protected VinueText(Context ctx, PlayerEntityModel model, float f) {
+	protected SneakNameText(Context ctx, PlayerEntityModel model, float f) {
 		super(ctx, model, f);
 	}
 	
 	@Inject(at = @At(value = "TAIL"), method = "updateRenderState")
 	public void updateRenderState(AbstractClientPlayerEntity player, PlayerEntityRenderState state, float f, CallbackInfo ci) {
-		if (!player.getUuid().equals(uuid))
+		if (SupportedServer.current() != SupportedServer.FLAGCLASH)
 			return;
 		MinecraftClient c = MinecraftClient.getInstance();
 		if (c.player != null && player.isInvisibleTo(c.player))
 			return;
-		state.nameLabelPos = new Vec3d(0, 2.4f, 0);
-		state.displayName = Text.literal("QuniaLover69").withColor(0xFFE35995);
+
+		boolean vinue = player.getUuid().equals(uuid);
+
+		if (player.isSneaking() || vinue) {
+			if (state.nameLabelPos == null)
+				state.nameLabelPos = new Vec3d(0, player.getStandingEyeHeight() + (vinue ? 0.8f : 0.1f), 0);
+			state.displayName = vinue
+				? Text.literal("QuniaLover69").withColor(0xFFE35995)
+				: player.getDisplayName().copy().withColor(0xFFFFFF);
+		}
 	}
 
 }
